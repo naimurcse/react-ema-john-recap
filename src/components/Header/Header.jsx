e252c22c8549fef78393b7b5a657bc822f97d15d
naimurcse <2015naim@gmail.com>
2023-11-19 08:30:23 +0600

    Markup of Header Section is done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <nav className="header__nav">
+                <div className="header__logo-box">
+                    <img src="/src/assets/images/Logo.svg" alt="" />
+                </div>
+                <ul className="header__menu">
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/shop">Shop</a></li>
+                    <li><a href="/inventory">Inventory Management</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,9 @@
+const Shop = () => {
+    return (
+        <div>
+            <h1>SHOP</h1>
+        </div>
+    );
+};
+
+export default Shop;
