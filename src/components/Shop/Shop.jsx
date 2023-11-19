e6bb66095cd72d5f48db52d18aaa7ed18ce7c752
naimurcse <2015naim@gmail.com>
2023-11-19 09:37:20 +0600

    Applied style in Order Summary Section of Shop

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,19 @@
+import "./Shop.css";
 const Shop = () => {
     return (
-        <div>
-            <h1>SHOP</h1>
-        </div>
+        <section className="shop">
+            <div className="shop__products">
+                <h1>PRODUCT</h1>
+            </div>
+            <div className="shop__order-summary">
+                <h3 className="shop__order-title">Order Summary</h3>
+                <p>Selected Items: 6</p>
+                <p>Total Price: $1140</p>
+                <p>Total Shipping Charge: $5</p>
+                <p>Tax: $114</p>
+                <h5 className="shop__grand-total">Grand Total: $1559</h5>
+            </div>
+        </section>
     );
 };
 
