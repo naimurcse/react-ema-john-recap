c9849b5a55a4bd76def18e9b021132348502bb2a
naimurcse <2015naim@gmail.com>
2023-11-19 10:12:01 +0600

    Display all product to the browser

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -11,23 +11,23 @@
 // export default Product;
 
 
-import React from 'react';
 import PropTypes from 'prop-types';
 import "./Product.css";
 
 const Product = (props) => {
+    const { img, name, price, quantity, ratings, seller, shipping } = props.product;
     console.log(props.product);
     return (
         <div className='product'>
             <div className="product__image">
-                <img src="" alt="" />
+                <img src={img} alt="" />
             </div>
-            
+
             <div className="product__body">
-                <h3 className="product__title">Hello Product</h3>
-                <p className='product__price'>Price: </p>
-                <p>Manufacturer: </p>
-                <p>Rating: </p>
+                <h3 className="product__title">{name}</h3>
+                <p className='product__price'>Price: {price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}</p>
             </div>
 
             <button className="btn btn-add-to-cart">Add to Cart</button>
