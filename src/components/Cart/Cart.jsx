6a9aec5d3a9a3d38e8302a866bcc1930af12756a
naimurcse <2015naim@gmail.com>
2023-11-19 18:54:30 +0600

    All the calculation of cart commponent are done and display

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,14 +2,28 @@ import PropTypes from 'prop-types';
 
 const Cart = (props) => {
     
+    
+
+    let totalPrice = 0;
+    let totalQuantity = 0;
+    let totalShipping = 0;
+    for (const product of props.cart) {
+        let productPrice = product.price;
+        totalPrice += productPrice * product.quantity;
+        totalQuantity += product.quantity;
+        totalShipping += product.shipping;
+    }
+
+    const tax = totalPrice * 15 / 100;
+    const grandTotal = totalPrice + tax + totalShipping;
     return (
         <div>
             <h3 className="shop__order-title">Order Summary</h3>
-            <p>Selected Items: {props.cart.length}</p>
-            <p>Total Price: $1140</p>
-            <p>Total Shipping Charge: $5</p>
-            <p>Tax: $114</p>
-            <h5 className="shop__grand-total">Grand Total: $1559</h5>
+            <p>Selected Items: {totalQuantity}</p>
+            <p>Total Price: {totalPrice}</p>
+            <p>Total Shipping Charge: {totalShipping}</p>
+            <p>Tax: {tax}</p>
+            <h5 className="shop__grand-total">Grand Total: {grandTotal}</h5>
         </div>
     );
 };
