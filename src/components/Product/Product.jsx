3093cd8e6f9c8d5c6df9f921bb39e8fcd4a02676
naimurcse <2015naim@gmail.com>
2023-11-20 09:30:56 +0600

    Display Product information in Product Component is done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,26 +3,32 @@
 import "./Product.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
-const Product = () => {
+
+const Product = (props) => {
+    console.log(props.product);
+    const {img,name,price,seller,ratings} = props.product;
     return (
-        <div>
-            <div className='product'>
-                <div className="product__image">
-                    <img src="" alt="" />
-                </div>
-
-                <div className="product__body">
-                    <h3 className="product__title">Name</h3>
-                    <p className='product__price'>Price: </p>
-                    <p>Manufacturer: </p>
-                    <p>Rating: </p>
-                </div>
-
-                <button className="btn btn-add-to-cart" >Add to Cart <FontAwesomeIcon icon={faCartShopping} /> </button>
+        <div className='product'>
+            <div className="product__image">
+                <img src={img} alt="" />
             </div>
+
+            <div className="product__body">
+                <h3 className="product__title">{name}</h3>
+                <p className='product__price'>Price: {[price]}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}</p>
+            </div>
+
+            <button className="btn btn-add-to-cart" >Add to Cart <FontAwesomeIcon icon={faCartShopping} /> </button>
         </div>
     );
+};
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired, // adjust the prop type based on your actual requirements
 };
 
 export default Product;
