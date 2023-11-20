242b12a45e5719e002545cf32a5898db5941b693
naimurcse <2015naim@gmail.com>
2023-11-20 09:20:10 +0600

    Product loaded and passed them by props

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,13 +1,26 @@
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
 import "./Shop.css";
+import { useDeferredValue, useEffect, useState } from "react";
 
 const Shop = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    },[])
+
     return (
         <div>
             <section className="shop">
                 <div className="shop__products">
-                    <Product></Product>
+                    {
+                        products.map(product => <Product key={product.id} product={product}></Product>)
+                    }
+                    
                 </div>
                 <div className="shop__order-summary">
                     <Cart></Cart>
