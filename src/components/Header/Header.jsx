4e387ad6de4519068c6c6b9dd483e83665944e84
naimurcse <2015naim@gmail.com>
2023-12-02 13:01:01 +0600

    set up react route and applied on nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,22 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <nav className="header__nav">
-                <div className="header__logo-box">
-                    <img src="/src/assets/images/Logo.svg" alt="" />
-                </div>
-                <ul className="header__menu">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/shop">Shop</a></li>
-                    <li><a href="/inventory">Inventory Management</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
-                </ul>
-            </nav>
-        </div>
-    );
+   return (
+      <div className="header">
+         <nav className="header__nav">
+            <div className="header__logo-box">
+               <img src="/src/assets/images/Logo.svg" alt="" />
+            </div>
+            <div className="header__menu">
+               <Link to="/home">Home</Link>
+               <Link href="/shop">Shop</Link>
+               <Link href="/inventory">Inventory Management</Link>
+               <Link href="/contact">Contact Us</Link>
+            </div>
+         </nav>
+      </div>
+   );
 };
 
 export default Header;
