a34666ecfa28631dd1bd4526e9cc562863be20dd
naimurcse <2015naim@gmail.com>
2023-12-03 01:46:13 +0600

    Displayed data to ReviewItem component

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./ReviewItem.css";
 
-const ReviewItem = () => {
+const ReviewItem = ({ product }) => {
+   console.log(product);
+
+   const { id, img, name, price, quantity } = product;
+
    return (
-      <div>
-         <h1>Reviwe Item</h1>
+      <div className="review-item">
+         <img src={img} alt="" />
+         <div className="review-item__details">
+            <div>
+               <h3 className="review-item__title">{name}</h3>
+               <p>
+                  Price: <span>${price}</span>
+               </p>
+               <p>
+                  Quantity: <span>{quantity}</span>
+               </p>
+            </div>
+            <button className="delete-btn">D</button>
+         </div>
       </div>
    );
 };
