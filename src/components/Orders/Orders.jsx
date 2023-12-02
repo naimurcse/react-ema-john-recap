025acc399ab52db8eacfac6ca13ac0283572b5c9
naimurcse <2015naim@gmail.com>
2023-12-03 01:18:41 +0600

    sent props to ReviewItem component to display cart data

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,6 +3,7 @@ import { getShoppingCartFromDb } from "../../utilities/fake";
 import Cart from "../Cart/Cart";
 import "./Order.css";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
    const cart = useLoaderData();
@@ -11,6 +12,9 @@ const Orders = () => {
       <div className="container order-cols">
          <div className="">
             <p>List: {cart.length}</p>
+            {cart.map((product) => (
+               <ReviewItem key={product.id} product={product}></ReviewItem>
+            ))}
          </div>
          <div className="">
             <h2>Order Summary</h2>
